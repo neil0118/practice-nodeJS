d9fde2acccf6d711f7627830f5e412d6a0b882b4
Neil Hu <hysh1991@gmail.com>
2020-07-20 14:13:25 +1000

    request body

--- app.js
+++ app.js
@@ -19,14 +19,27 @@ const server = http.createServer((req, res) => {
     res.write("<html>");
     res.write("<head><title>My First Page</title></head>");
     res.write(
-      "<body><form action='/message' method='POST'><input type='text'/><button type='submit'>Submit</button></form></body>"
+      `<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Submit</button></form></body>`
     );
     res.write("</html>");
     return res.end();
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "dummy");
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+
+      fs.writeFileSync("message.txt", message);
+    });
+
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
