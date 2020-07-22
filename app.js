c92bd60cf6f2a4ebf37763968bceca339b1de3fe
Neil Hu <hysh1991@gmail.com>
2020-07-22 23:58:36 +1000

    pug templates

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404",{pageTitle:"404 Not Found"});
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
