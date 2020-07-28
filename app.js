c948ea4f9f5b0a5375906cfd8bb25398329566c3
Neil Hu <hysh1991@gmail.com>
2020-07-29 02:49:15 +1000

    set up connection to local mongoDB

--- app.js
+++ app.js
@@ -3,21 +3,12 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const errorController = require("./controllers/error");
-const sequelize = require("./util/database");
-const Product = require("./models/products");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+const mongoConnect = require("./util/database");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
-
-//db.auth("useradmin", "adminpassword")
-//const MONGO_URI = 'mongodb://localhost:27017/auth';
 
 //compile with EJS engine
 app.set("view engine", "ejs");
@@ -27,12 +18,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => {});
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  next();
+  // })
+  // .catch((err) => {});
 });
 
 app.use("/admin", adminRoutes);
@@ -40,38 +31,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-Product.belongsToMany(Order, { through: OrderItem });
-
-sequelize
-  .sync()
-  // .sync({ force: true })
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Neil", email: "hello@world.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    user.getCart().then((cart) => {
-      if(!cart)
-        return user.createCart();
-    });
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoConnect((client) => {
+  app.listen(3000);
+});
--- util/database.js
+++ util/database.js
@@ -1,8 +1,26 @@
-const Sequelize = require("sequelize");
+// const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("node-complete", "appuser", "password", {
-  dialect: "mysql",
-  host: "localhost",
-});
+// const sequelize = new Sequelize("node-complete", "appuser", "password", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodb = require("mongodb");
+//db.auth("useradmin", "adminpassword")
+//const MONGO_URI = 'mongodb://localhost:27017/auth';
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect("mongodb://localhost:27017/node-complete")
+    .then((result) => {
+      console.log("Connected!");
+      callback(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = mongoConnect;
