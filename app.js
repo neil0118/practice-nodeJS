8d5e6d19ae198e9e10563cf2d057ff5dd1980fab
Neil Hu <hysh1991@gmail.com>
2020-07-30 02:19:34 +1000

    mongoDB dont need a cart collection/ append cart to user collection add product to cart

--- app.js
+++ app.js
@@ -21,9 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("5f2193fc1279000029000642")
     .then((user) => {
-      req.user = user;
-      console.log("username: ", user.name);
-      console.log("email: ", user.email);
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => {});
--- models/user.js
+++ models/user.js
@@ -3,14 +3,49 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    let cartProductIndex;
+    let updatedCartItems = [];
+    if (this.cart) {
+      updatedCartItems = [...this.cart.items];
+      cartProductIndex = this.cart.items.findIndex((item) => {
+        return item.productId.toString() === product._id.toString();
+      });
+    }
+    let newQuantity = 1;
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      console.log(1);
+      updatedCartItems.push({
+        productId: new mongodb.ObjectID(product._id),
+        quantity: newQuantity,
+      });
+    }
+
+    const updatedCart = {
+      items: updatedCartItems,
+    };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectID(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
