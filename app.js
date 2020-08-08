318e203ef349136eb5f9bf17a18c43304bb74eea
Neil Hu <hysh1991@gmail.com>
2020-08-09 01:34:55 +1000

    login view, form, controller

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const User = require("./models/user");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
