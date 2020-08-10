aa021c62af0768d04019538ee6bd7960d8c0f008
Neil Hu <hysh1991@gmail.com>
2020-08-10 15:49:26 +1000

    npm install express-session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn =
+    req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "LoggedIn=true");
+  res.redirect("/");
 };
--- controllers/shop.js
+++ controllers/shop.js
@@ -8,6 +8,7 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: "All Product",
         path: "/products",
+        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch((err) => {
@@ -24,6 +25,7 @@ exports.getProduct = (req, res, next) => {
         product: product,
         pageTitle: product.title,
         path: `/products`,
+        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch((err) => {});
@@ -36,6 +38,7 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: "shop",
         path: "/",
+        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch((err) => {
@@ -53,6 +56,7 @@ exports.getCart = (req, res, next) => {
         path: "/cart",
         pageTitle: "Your Cart",
         products: products,
+        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch((err) => {});
@@ -116,6 +120,7 @@ exports.getOrders = (req, res, next) => {
       path: "/orders",
       pageTitle: "Your Orders",
       orders: orders,
+      isAuthenticated: req.isLoggedIn,
     });
   });
 };
@@ -124,5 +129,6 @@ exports.getCheckout = (req, res, next) => {
   res.render("/shop/checkout", {
     path: "/checkout",
     pageTitle: "Checkout",
+    isAuthenticated: req.isLoggedIn,
   });
 };
